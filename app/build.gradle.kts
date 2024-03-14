bff6a684c8f89f1644629aae0dd442baf408ee85
Daniel <kuanshenwang@gmail.com>
2024-03-14 11:12:57 +0800

    新增 Retrofit lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -39,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -47,4 +47,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // retrofit
+    implementation("com.squareup.retrofit2:converter-moshi:${project.ext.get("retrofitVersion")}")
+    implementation("com.squareup.moshi:moshi:${project.ext.get("moshiVersion")}")
+    kapt ("com.squareup.moshi:moshi-kotlin-codegen:${project.ext.get("moshiVersion")}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${project.ext.get("okhttpInterceptorVersion")}")
+    implementation("com.github.bumptech.glide:glide:${project.ext.get("glid")}") {
+        exclude("com.android.support")
+    }
+    implementation ("androidx.activity:activity-ktx:${project.ext.get("activityKtxVersion")}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
+import com.github.javaparser.utils.Utils.set
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+allprojects {
+    ext {
+        set("retrofitVersion", "2.9.0")
+        set("moshiVersion", "1.11.0")
+        set("okhttpInterceptorVersion", "4.9.0")
+        set("glid", "4.11.0")
+        set("activityKtxVersion", "1.2.0-rc01")
+    }
 }
