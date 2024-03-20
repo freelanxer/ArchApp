a780426f8817c8091dbfdf926987f5c12b849461
Daniel <kuanshenwang@gmail.com>
2024-03-20 14:55:58 +0800

    BaseActivity 加入 toast 功能

--- app/src/main/java/com/freelanxer/archapp/ui/base/BaseActivity.kt
+++ app/src/main/java/com/freelanxer/archapp/ui/base/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.freelanxer.archapp.ui.base
 
+import android.content.res.Resources.NotFoundException
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -14,4 +16,20 @@ abstract class BaseActivity : AppCompatActivity() {
         initViewBinding()
         observeViewModel()
     }
+
+    protected fun showToast(text: Any) {
+        when (text) {
+            is String -> Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            is Int -> {
+                val resString: String
+                try {
+                    resString = getString(text)
+                } catch (_ : NotFoundException) {
+                    return
+                }
+                Toast.makeText(this, resString, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
