5c14c1e95045abe6f7d5061876f231b2e1ea9330
Daniel <kuanshenwang@gmail.com>
2024-08-28 15:32:06 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import com.github.javaparser.utils.Utils.set
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     // Add the dependency for the App Distribution Gradle plugin
     id("com.google.firebase.appdistribution") version "5.0.0" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
 //    id("com.google.devtools.ksp") version "1.9.10" apply false
 }
