e9cef9140a7294a7a90b2fbfcfbd5ac0987d89a1
Daniel <kuanshenwang@gmail.com>
2024-08-28 16:34:34 +0800

    Fix hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-//    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
     // Add the App Distribution Gradle plugin
@@ -74,7 +73,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi:1.11.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+//    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.github.bumptech.glide:glide:4.11.0") {
         exclude("com.android.support")
@@ -82,7 +81,7 @@ dependencies {
     implementation ("androidx.activity:activity-ktx:1.2.0-rc01")
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
 //    kspAndroidTest("com.google.dagger:hilt-android-compiler:2.48")
     // Coil
@@ -93,6 +92,6 @@ dependencies {
 }
 
 // Allow references to generated code
-//kapt {
-//    correctErrorTypes = true
-//}
+kapt {
+    correctErrorTypes = true
+}
